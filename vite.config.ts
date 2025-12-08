f101a984c0adef65c3b912af0f535ddb8cd303fd
Lukas Villumsen <lukas.villumsen@bettercloud.com>
2025-12-08 13:24:37 +0100

    build: explicit build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
   server: {
     port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
   },
 });
